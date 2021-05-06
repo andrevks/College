e4fe75b8c4fd41cadd3cf452e68f34d1e25f03f3
andrevks <italiabk@gmail.com>
2021-05-06 00:06:37 -0400

    build: first arm assembly code

---
+++ Compilers/assemblyARM/programaEscritaAndre1.s
@@ -0,0 +1,14 @@
+.global _start
+
+_start:
+    MOV R7, #4
+    MOV R0, #1
+    LDR R1, =string
+    LDR R2, =len_string
+    SWI 0
+_end:
+    MOV R7, #1
+    SWI 0
+_data
+  string: .ascii "Voce esta sendo vigiado(a) !\n"
+  len_string = .-string
