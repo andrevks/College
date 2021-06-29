fdd518d28f0f36e3d74620dc1743b6220dc51eba
andrevks <italiabk@gmail.com>
2021-06-28 20:13:08 -0400

    fix: cleaner code

--- Compilers/assemblyARM/calcAndre.s
+++ Compilers/assemblyARM/calcAndre.s
@@ -1,21 +1,27 @@
 .data 
 .balign 4
-question: .asciz "Escolha a operação (SOM:1,SUB:2,MUL:3,DIV:4)\n"
+question: .asciz "\nEscolha a operação ( SOMA:1 | SUB:2 | MULT:3 | DIV:4 )\n"
 
 .balign 4
-op: .asciz "DIV: (%d/%d) =: %d\n" 
+option_text: .asciz "Opção: \n"
 
 .balign 4
-result1: .asciz "Resultado da operação: %d + %d = %d\n"
+number1_text: .asciz "1º Numero: \n"
 
 .balign 4
-result2: .asciz "Resultado da operação: %d - %d = %d\n"
-
-@ .balign 4
-@ result3: .asciz "Resultado da operação: %d + %d = %d\n"
+number2_text: .asciz "2º Numero: \n"
 
 .balign 4
-result4: .asciz "Resultado da operação: %d / %d = %d\n"
+result1: .asciz "Resultado da Soma: %d + %d = %d\n"
+
+.balign 4
+result2: .asciz "Resultado da Subtração: %d - %d = %d\n"
+
+.balign 4
+result3: .asciz "Resultado da Multiplicação: %d * %d = %d\n"
+
+.balign 4
+result4: .asciz "Resultado da Divisão: %d / %d = %d\n"
 
 .balign 4
 option: .word 0
@@ -27,16 +33,10 @@ number1: .word 0
 number2: .word 0
 
 .balign 4
-resultvalue: .word 0
-
-.balign 4
 pattern: .asciz "%d"
 
 .balign 4
 backup_lr: .word 
-
-.balign 4
-backup_lr1: .word
 
 .text
 .global main
@@ -49,15 +49,25 @@ _start:
   @Print Initial string
   LDR R0, addr_question @Storing the string in question
   BL printf @jump and come back
-  @scanf:
+
+_key_input:
+  LDR R0, addr_option_text @Storing the string in question
+  BL printf @jump and come back
+
   LDR R0, addr_pattern @%d
   LDR R1, addr_option  @value stored from the keyboard
   BL scanf
+
+  LDR R0, addr_number1_text @Storing the string in question
+  BL printf @jump and come back
 
   LDR R0, addr_pattern @%d
   LDR R1, addr_number1  @value stored from the keyboard
   BL scanf
   
+  LDR R0, addr_number2_text @Storing the string in question
+  BL printf @jump and come back
+
   LDR R0, addr_pattern @%d
   LDR R1, addr_number2  @value stored from the keyboard
   BL scanf
@@ -92,26 +102,27 @@ _sub:
   BAL _result2
 
 _mult:
+  MUL R3, R0, R1
+  BAL _result3
 
 _div:
-  @R3 = result
   @R1 = divisor
-  @R0 = dividend
   CMP R0, R1
   SUB R0, R0, R1
   ADD R3, R3, #1 @incr result
   BHI _div
   BAL _result4
   
-
-
-
 _result1:
   LDR R0, addr_result1 @String Result: %d
   BAL _print_result
 
 _result2:
   LDR R0, addr_result2 @String Result: %d
+  BAL _print_result
+
+_result3:
+  LDR R0, addr_result3 @String Result: %d
   BAL _print_result
 
 _result4:
@@ -132,16 +143,19 @@ _end:
   BX lr @Return for the main
 @Address for the data:
 addr_question: .word question
-addr_op: .word op
+addr_option_text: .word option_text
+addr_number1_text: .word number1_text
+addr_number2_text: .word number2_text 
 addr_pattern: .word pattern
 addr_option: .word option
 addr_number1: .word number1
 addr_number2: .word number2
-addr_resultvalue: .word resultvalue
 addr_result1: .word result1
 addr_result2: .word result2
+addr_result3: .word result3
 addr_result4: .word result4
 addr_backup_lr: .word backup_lr
+
 
 .global printf
 .global scanf
