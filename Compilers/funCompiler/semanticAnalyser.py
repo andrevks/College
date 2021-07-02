5e19b5fb0bd5189a7b69d505e9cc0e0de3e021d7
andrevks <italiabk@gmail.com>
2021-07-02 11:56:50 -0400

    fix: allow just TK_ID

--- Compilers/funCompiler/semanticAnalyser.py
+++ Compilers/funCompiler/semanticAnalyser.py
@@ -33,6 +33,9 @@ class SemanticAnalyser:
     def insert(self , lexeme, line, value , location):
         new_var = Symbol(lexeme,value,line,location)
         print("New VAR: ",new_var.var)
+        print("line: ", new_var.line)
+        print("location: ",new_var.location)
+        print("---------------------------\n")
         key = new_var.var
         self.__symbol_table.insert(key,new_var)
 
@@ -44,10 +47,26 @@ class SemanticAnalyser:
 
     def verify_semantic(self):
         l_index = 0
-        for token in self.__token_list:
-            # print(token.lexeme)
-            # print(token.line)
-            # print(token.type)
-            # print("---------\n")
-            self.insert(token.lexeme,token.line,0,l_index)
+        tok = self.__token_list
+        while l_index < len(tok):
+            lex = tok[l_index].lexeme
+            line = tok[l_index].line
+            type = tok[l_index].type
+
+            #DECLARAÇÃO DE VARIÁVEL
+            if type == 'funny':
+                #put the variables on the table
+                l_index_bu = l_index + 1
+                while tok[l_index_bu].lexeme != '.' :
+                    if tok[l_index_bu].type == 'TK_ID':
+                        lex = tok[l_index_bu].lexeme
+                        line = tok[l_index_bu].line
+                        self.insert(lex,line,0,l_index_bu)
+                    l_index_bu += 1
+                # continue
+            elif type == 'TK_ID':
+                var = lex
+                if not self.is_var_on_table(var):
+                    print("ERRO SEMÂNTICO !")
+                # continue
             l_index += 1
