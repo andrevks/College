4d90ab825977bbf6f1bd4eb60aece830e8bd4656
andrevks <italiabk@gmail.com>
2021-07-02 16:02:52 -0400

    fix: variable declarion error

--- Compilers/funCompiler/semanticAnalyser.py
+++ Compilers/funCompiler/semanticAnalyser.py
@@ -13,7 +13,7 @@ class SemanticError(Exception):
         print(f'Tipo de erro: "ERRO SEMÂNTICO" \n'
               f'Linha {self.__line} e Coluna {self.__col}\n'
               f'Descrição: {self.__msg}\n'
-              f'Token \'  {self.__lex}  \'')
+              f'Variável: \'  {self.__lex}  \'')
         print('---------------------------------------------------\n')
 
 class Symbol:
@@ -32,29 +32,21 @@ class SemanticAnalyser:
 
     def insert(self , lexeme, line, value , location):
         new_var = Symbol(lexeme,value,line,location)
-        print("\n-----------INSERT------------")
+        print("Inserir variável")
         print("VARIÁVEL A SER DECLARADA: ",new_var.var)
-        print("line: ", new_var.line)
-        print("location: ",new_var.location)
-        print("-----------------------------\n")
         key = new_var.var
         self.__symbol_table.insert(key,new_var)
 
     def is_var_on_table(self, key):
-        print("\n-----------TABLE------------")
-        print("Encontrar chave: ", key)
         check = False
         try:
             print("Está na tabela ? ", self.__symbol_table.find(key).var == key)
-            print("-----------------------------\n")
+            print("Encontrar Variavel")
             check = self.__symbol_table.find(key).var == key;
             return check
         except AttributeError as att:
             raise
             check = False
-
-    # def verify(self):
-
 
     def verify_semantic(self):
         l_index = 0
@@ -68,28 +60,28 @@ class SemanticAnalyser:
             #DECLARAÇÃO DE VARIÁVEL
             if type == 'funny':
                 #put the variables on the table
+                print("\nDECLARAR VARIÁVEL")
                 l_index_bu = l_index + 1
                 while tok[l_index_bu].lexeme != '.' :
 
                     if tok[l_index_bu].type == 'TK_ID':
                         lex = tok[l_index_bu].lexeme
                         line = tok[l_index_bu].line
-                        self.insert(lex,line,0,l_index_bu)
+                        #se ta na tabela é != None
+                        if self.__symbol_table.find(lex) == None :
+                            self.insert(lex,line,0,l_index_bu)
+                        else:
+                            SemanticError(line , col , lex , f'Variável Duplicada')
                     elif tok[l_index_bu].type == 'TK_ATRIB':
                         break
                     l_index_bu += 1
-                # continue
             elif type == 'TK_ID':
-                var = lex
-                # if not self.is_var_on_table(var):
-                #     print("ERRO SEMÂNTICO !")
-                print("\n----------ENCONTROU VAR-------------")
-                print("LEX antes da checagem: ", lex)
+                print("\nENCONTROU VARIÁVEL")
                 try:
+                    var = lex
                     if self.is_var_on_table(var):
                         print("Variável foi declarada")
                 except AttributeError as att:
-                    SemanticError(line , col , lex ,f'Variável NÃO declarada: ( {att} )')
-                    print("-----------------------------\n")
-                # continue
+                    SemanticError(line , col , lex ,f'Variável NÃO Declarada: ( {att} )')
+
             l_index += 1
