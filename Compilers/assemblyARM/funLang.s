0cf4e270b58581f29a80fdb8815d2d525374b71a
andrevks <italiabk@gmail.com>
2021-07-15 20:37:35 -0400

    build: funlang assembly

--- Compilers/assemblyARM/funLang.s
+++ Compilers/assemblyARM/funLang.s
@@ -1,101 +1,74 @@
-.data
-.balign 4
-backup_lr: .word
-
-.balign 4
-pattern: .asciz "%d"
-
-.balign 4
-variable: .word 0  
-
-.balign 4
-variable2: .word 1
-
-.balign 4
-string: .asciz "Primeira var: %d %d\n" 
-
-.balign 4
-string2: .asciz "Segunda var\n" 
-
-.balign 4
-question: .asciz "Digite um numero: \n"
-
-.balign 4
-number1: .word 0
-
-.text
 .global main 
 .func main 
 main:
-  @here the LR(Link Register) is stored in a variable
-  LDR R1, addr_backup_lr @Address for the linkRegister
-  STR lr, [R1] @Store value to R1
+  @ @here the LR(Link Register) is stored in a variable
+  LDR R1, =backup_lr @Address for the linkRegister
+  STR LR, [R1] @Store value to R1
+  @ PUSH {IP,LR}
   @------BEGIN--------
-_start:
-  @Print Initial string
-  LDR R0, addr_question @Storing the string in question
+
+  @ESCREVA String
+  LDR R0, =question @Storing the string in question
   BL printf @jump and come back
-  @LEIA and put in the variable
 
-  LDR R0, addr_pattern
-  LDR R1, addr_variable1
+  @LEIA Var
+  LDR R0, =pattern
+  LDR R1, =variable
   BL scanf
 
-  LDR R0, addr_pattern
-  LDR R1, addr_variable2
-  BL scanf
-  @ @ATRIB of two var
-  @ LDR R1, addr_variable
-  @ STR R0, [R1]
-
-  @ESCREVA string
-  @ LDR R0, addr_string @string text
-  @ BL printf
-
-  @ESCREVA var
-  @ LDR R0, addr_string
-  @ LDR R1, addr_variable
-  @ LDR R1, [R1]
-  @ BL printf
-
-_print_result:
-  LDR R0, addr_string
-  LDR R1, addr_variable1
+  @ATRIB Var
+  LDR R1,=result @after some calculation (sum,sub...)
   LDR R1, [R1]
-  LDR R2, addr_variable2
-  LDR R2, [R2]
+  LDR R0,=variable
+  STR R1, [R0]
+
+  @ATRIB Num
+  MOV R0, #1
+  LDR R1, =variable2
+  STR R0, [R1]
+
+  @ESCREVA STRING
+  LDR R0, =string
   BL printf
 
-  @ @ESCREVA string
-  @ LDR R0, addr_string2 @string text
-  @ BL printf
+  @ESCREVA VAR
+  LDR R0, =pattern_print
+  LDR R1, =variable
+  LDR R1, [R1]
+  BL printf
 
-  @ @ESCREVA var
-  @ LDR R0, addr_pattern
-  @ LDR R1, addr_variable2
-  @ BL printf
+  @ESCREVA STRING
+  LDR R0, =string2
+  BL printf
 
-  @ LDR R1, addr_variable2
-  @ LDR R1, [R1]
-  @ ADD R0, R0, R1 
+  @ESCREVA VAR
+  LDR R0, =pattern_print
+  LDR R1, =variable2
+  LDR R1, [R1]
+  BL printf
 
 
   @-------END---------
 _end:
-  LDR lr, addr_backup_lr @End func, reset LR
-  LDR lr, [lr]
-  BX lr @Return from the main
+  LDR LR, =backup_lr @End func, reset LR
+  LDR LR, [LR]
+  BX LR @Return from the main
+  @ POP {IP,LR}
 
-@Default var address
-addr_backup_lr: .word backup_lr
-addr_pattern: .word pattern
-@Var address
-addr_variable: .word variable 
-addr_variable2: .word variable2
-addr_number1: .word number1
-addr_string: .word string
-addr_string2: .word string2
-addr_question: .word question
+.data
+.balign 8
+@default
+pattern: .asciz "%d"
+pattern_print: .asciz "%d\n"
+backup_lr: .word 0
+@Data definition
+variable: .word 0  
+variable2: .word 0
+result: .word 100
+string: .asciz "Primeira Var:" 
+string2: .asciz "Segunda var:" 
+question: .asciz "Digite um numero:\n"
+number1: .word 0
 
 .global printf
 .global scanf
