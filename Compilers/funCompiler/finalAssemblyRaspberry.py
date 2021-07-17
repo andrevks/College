3d2ffe7e0dc79484c5d1f6fd870bd958b6f84fe3
andrevks <italiabk@gmail.com>
2021-07-16 22:30:27 -0400

    build: few changes

--- Compilers/funCompiler/finalAssemblyRaspberry.py
+++ Compilers/funCompiler/finalAssemblyRaspberry.py
@@ -13,6 +13,14 @@ class FinalRaspberry:
         l_index = 0
 
         print('\n____FINAL:')
+
+        self.append_final_code('.global main')
+        self.append_final_code('.func main')
+        self.append_final_code('main:')
+        self.append_final_code('LDR R1, =backup_lr', '@Address of the var ')
+        self.append_final_code('STR LR, [R1] ', '@Store LinkRegister in var ')
+        self.__final_code.append('@------BEGIN--------\n')
+
         while l_index < len(inter_code):
             inter_line = inter_code[l_index].split()[0]
 
@@ -20,10 +28,23 @@ class FinalRaspberry:
                 inter_line = inter_code[l_index].split()[1]
                 self.append_final_code('LDR R0, =pattern', '@ int pattern (%d)')
                 self.append_final_code(f'LDR R1, ={inter_line}', '@Send variable address')
-                self.append_final_code(f'BL, scanf', ' @Function to receive input\n')
+                self.append_final_code(f'BL scanf', ' @Function to receive input\n')
 
             l_index += 1
 
         print("\n-----------FINAL CODE-------------")
+        self.__final_code.append('@------END--------')
+        self.append_final_code('LDR R1, =backup_lr', '@Address of the var ')
+        self.append_final_code('LDR LR, [LR]')
+        self.append_final_code('BX LR', 'Return from the main function')
+
+        self.append_final_code('\n.data', '@ Data section')
+        self.append_final_code('.balign 8', '@Bytes allocated to each var')
+        self.append_final_code(f'pattern: .asciz \"%d\"', '@Int format')
+        self.append_final_code(f'pattern_print: .asciz \"%d\\n\"')
+        self.append_final_code(f'backup_lr: .word 0\n')
+        for var in self.__var_list:
+            self.append_final_code(f'{var}: .word 0',)
+
         for line in self.__final_code:
             print(line)
