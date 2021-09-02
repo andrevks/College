40a06c9ce4d4d75c7d4ea48be25af22ac31643c0
andrevks <italiabk@gmail.com>
2021-09-01 21:13:26 -0400

    build: led01, my version new led sequence

--- Microcontrolers/led01.c
+++ Microcontrolers/led01.c
@@ -30,14 +30,164 @@ void main()
    setup_comparator(NC_NC_NC_NC);
    setup_vref(FALSE);
    
+  //  void turn_on_off_led(int timeMs, int pinNum, char pinLetter){
+
+  //     char pinName[15];
+  //     strcpy(pinName, "Pin_");
+  //     strcat(pinName, pinLetter);
+
+  //     strcat(pinName, pinNum);
+
+  //    output_high(pinName );
+  //    delay_ms( timeMs );
+  //    output_low( pinName );
+  //    delay_ms( timeMs );
+  //  }
+   
     while(true){
-     output_high(PIN_D2);
-
-     delay_ms( 1000 );//1 sec (1000 milsec)
-
+    //Reset LEDs to LOW
+     output_low(PIN_D0);
+     output_low(PIN_D1);
      output_low(PIN_D2);
-     
+     output_low(PIN_D3);
+     output_low(PIN_D4);
+     output_low(PIN_D5);
+     output_low(PIN_D6);
+     output_low(PIN_D7);
+    //----------------
+
+
+
+     output_high(PIN_D0);
      delay_ms( 1000 );//1 sec (1000 milsec)
+     output_low(PIN_D0);
+     delay_ms( 1000 );//1 sec (1000 milsec)
+
+     output_high(PIN_D4);
+     delay_ms( 1000 );//1 sec (1000 milsec)
+     output_low(PIN_D4);
+     delay_ms( 1000 );//1 sec (1000 milsec)
+
+     output_high(PIN_D7);
+     delay_ms( 1000 );//1 sec (1000 milsec)
+     output_low(PIN_D7);
+     delay_ms( 1000 );//1 sec (1000 milsec)
+
+    //----------------
+     output_high(PIN_D0);
+     delay_ms( 600 );//1 sec (1000 milsec)
+     output_low(PIN_D0);
+     delay_ms( 600 );//1 sec (1000 milsec)
+
+     output_high(PIN_D1);
+     delay_ms( 600 );//1 sec (1000 milsec)
+     output_low(PIN_D1);
+     delay_ms( 600 );//1 sec (1000 milsec)
+
+     output_high(PIN_D2);
+     delay_ms( 600 );//1 sec (1000 milsec)
+     output_low(PIN_D2);
+     delay_ms( 600 );//1 sec (1000 milsec)
+
+     output_high(PIN_D3);
+     delay_ms( 600 );//1 sec (1000 milsec)
+     output_low(PIN_D3);
+     delay_ms( 600 );//1 sec (1000 milsec)
+
+     output_high(PIN_D6);
+     delay_ms( 650 );//1 sec (1000 milsec)
+     output_low(PIN_D6);
+     delay_ms( 300 );//1 sec (1000 milsec)
+
+     output_high(PIN_D6);
+     delay_ms( 200 );//1 sec (1000 milsec)
+     output_low(PIN_D6);
+     delay_ms( 650 );//1 sec (1000 milsec)
+
+    //----------------
+     output_high(PIN_D7);
+     delay_ms( 547 );
+     output_low(PIN_D7);
+     delay_ms( 123 );
+
+     output_high(PIN_D6);
+     delay_ms( 750 );
+     output_low(PIN_D6);
+     delay_ms( 750 );
+     
+     output_high(PIN_D4);
+     delay_ms( 750 );
+     output_low(PIN_D4);
+     delay_ms( 750 );
+
+    //----------------
+
+     output_high(PIN_D4);
+     delay_ms( 500 );
+     output_low(PIN_D4);
+     delay_ms( 500 );
+
+     output_high(PIN_D6);
+     delay_ms( 800 );
+     output_low(PIN_D6);
+     delay_ms( 800 );
+
+     output_high(PIN_D4);
+     delay_ms( 500 );
+     output_low(PIN_D4);
+     delay_ms( 500 );
+
+     output_high(PIN_D1);
+     delay_ms( 800 );
+     output_low(PIN_D1);
+     delay_ms( 800 );
+
+     output_high(PIN_D2);
+     delay_ms( 800 );
+     output_low(PIN_D2);
+     delay_ms( 800 );
+     
+     output_high(PIN_D5);
+     delay_ms( 800 );
+     output_low(PIN_D5);
+     delay_ms( 800 );
+    //----------------
+     output_high(PIN_D0);
+     delay_ms( 300 );
+     output_high(PIN_D1);
+     delay_ms( 300 );
+     output_high(PIN_D2);
+     delay_ms( 300 );
+     output_high(PIN_D3);
+     delay_ms( 300 );
+     output_high(PIN_D4);
+     delay_ms( 300 );
+     output_high(PIN_D5);
+     delay_ms( 300 );
+     output_high(PIN_D6);
+     delay_ms( 300 );
+     output_high(PIN_D7);
+     delay_ms( 300 );
+    //----------------
+
+     output_low(PIN_D0);
+     delay_ms( 300 );
+     output_low(PIN_D1);
+     delay_ms( 300 );
+     output_low(PIN_D2);
+     delay_ms( 300 );
+     output_low(PIN_D3);
+     delay_ms( 300 );
+     output_low(PIN_D4);
+     delay_ms( 300 );
+     output_low(PIN_D5);
+     delay_ms( 300 );
+     output_low(PIN_D6);
+     delay_ms( 300 );
+     output_low(PIN_D7);
+     delay_ms( 300 );
+
+
    }
 
 
