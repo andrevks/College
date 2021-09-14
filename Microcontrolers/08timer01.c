9b69a152c75a64926a340de284f9daedecb7947e
André <39144691+andrevks@users.noreply.github.com>
2021-09-14 08:26:20 -0400

    update: new count

--- Microcontrolers/08timer01.c
+++ Microcontrolers/08timer01.c
@@ -1,18 +1,18 @@
 //#include <08timer01.h>
 
 /*
-   -  Aluno:André Geraldo, Eng. da Computação (7º semestre).
+   -  Aluno:AndrÃ© Geraldo, Eng. da Computaï¿½ï¿½o (7ï¿½ semestre).
    -  Professor Dr. Alberto Willian Mascarenhas.
    
    -  Disciplina: Microcontroladores.
    
    -  Objetivo: Crie um programa para o PIC16F877A para que os 3 LEDs (1, 2 e 3) conectados na porta D (D0, D1
-      e D2) respectivamente, liguem e desliguem com frequências iguais Na (50), Nb(280) e Nc(700) Hertz
+      e D2) respectivamente, liguem e desliguem com frequï¿½ncias iguais Na (50), Nb(280) e Nc(700) Hertz
       
    - Data: 14/09/2021
 */
 #include <16F877A.h>
-#device ADC=16
+#device ADC=8
 
 #FUSES NOWDT                    //No Watch Dog Timer
 #FUSES NOBROWNOUT               //No brownout reset
@@ -27,30 +27,33 @@ unsigned int16 count[3] = {0, 0, 0};
 void  RTCC_isr(void) 
 {
   /*
-      Para chegar aos valores foi preciso pegar as frequências dadas e converter-las 
-      em período.
+      Para chegar aos valores foi preciso pegar as frequencias dadas e converter-las 
+      em periodo.
       
-      Por exemplo, Na = 50 então Ta = 1/50 => Ta = 20ms
+      Por exemplo, Na = 50 entÃ£o Ta = 1/50 => Ta = 20ms
       
-      - Depois tem que lembrar que só a metade do período que é mantido ligado.
-      - Deve-se considerar que o tempo de overflow de 13.1ms.
-      - Overflow = 0.2us * 256 * DIV  | No caso o DIV aqui é 256
-      - O count então varia de acordo com o overflow
-      - Count = (T/2)*1000/OVF
-      
+      - O LED eh mantido ligado com a metade do periodo.
+      - Deve-se considerar o tempo de overflow de 13.1ms, neste caso.
+      - Overflow = 0.2us * 256 * DIV  | No caso o DIV aqui eh 256
+      - O count entao varia de acordo com o overflow.
+      - Assim, o count = (T/2)*1000/OVERFLOW
+      - Com Ta = 20ms, tem-se count = (10)*1000/13.1 => count = 763
    */
+   count[0]++;
+   count[1]++;
+   count[2]++;
 
-   if( ++count[0] >= 763){
+   if( count[0] >= 763){
       output_toggle(PIN_D0);
       count[0] = 0;
    }
   
-   if( ++count[1] >= 136){
+   if( count[1] >= 136){
       output_toggle(PIN_D1);
        count[1] = 0;
    } 
    
-   if( ++count[2] >= 55){
+   if( count[2] >= 55){
       output_toggle(PIN_D2);
        count[2] = 0;
    }
@@ -60,7 +63,7 @@ void  RTCC_isr(void)
 
 void main()
 {
-   setup_timer_0(RTCC_INTERNAL|RTCC_DIV_256|RTCC_8_bit);      //13.1 ms overflow
+   setup_timer_0(RTCC_INTERNAL|RTCC_DIV_256|RTCC_8_bit);//13.1 ms overflow
 
 
    enable_interrupts(INT_RTCC);
